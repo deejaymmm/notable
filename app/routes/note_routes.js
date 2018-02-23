02c6fdc3112d6568b628e0a02b92231b8420fe51
deejay_mmm <djmmm@ukr.net>
2018-02-23 17:53:20 +0200

    first commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,23 @@
+/*module.exports = function(app, db) {
+};*/
+
+// CREATE route
+module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
+
+/*module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.send('Hello')
+  });
+};*/
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : 'mongodb://user:123456@ds245228.mlab.com:45228/notable'
+};
